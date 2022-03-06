bc83d4f57c4cdfc3156e889b80f25cc65d0531f2
Dominik Giroux <giroux.dominik@gmail.com>
2022-03-05 22:55:31 -0500

    removed useless file and updated loader bg color

--- src/js/theme.js
+++
@@ -1,8 +0,0 @@
-// Loading
-window.addEventListener("DOMContentLoaded", event => {
-  setTimeout(() => {
-    const loader = document.getElementById("loading");
-    loader.style.visibility = "hidden";
-    loader.style.opacity = "0";
-  }, 500);
-});
